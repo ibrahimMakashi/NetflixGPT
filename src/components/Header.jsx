fe05ffc127b72e4ecca9f3bfa41c924f27a3ef1d
ibrahimMakashi <ibrahimmakashi5159@gmail.com>
2025-05-19 17:14:44 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,14 +40,16 @@ const Header = () => {
   return (
     <div className="header-browse">
       <div className="browse-header flex">
-        <img
-          src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-          alt=""
-        />
-        <div className="browse-header-right flex">
+        <div className="flex">
+          <img
+            src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+            alt=""
+          />
           <button onClick={handleGPTPage} className="gpt-btn">
-            {!isGpt ? "GPT search - Ai" : "Home page"}
+            {!isGpt ? "GPT Ai search" : "Home page"}
           </button>
+        </div>
+        <div className="browse-header-right flex">
           <img src={user?.photoURL} alt="user" />
           <p>{user?.displayName}</p>
           <button onClick={handleSignOut}>Sign Out</button>
